d818df6eee5e1a1497282405e136d8f623300b9f
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-04-18 19:31:59 +0200

    Create EmployeeCard component

---
+++ packages/ui/components/EmployeeCard/EmployeeCard.stories.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { ComponentStory, type ComponentMeta } from "@storybook/react";
+import EmployeeCard from "./EmployeeCard";
+
+export default {
+  title: "EmployeeCard",
+  component: EmployeeCard,
+} as ComponentMeta<typeof EmployeeCard>;
+
+const Template: ComponentStory<typeof EmployeeCard> = (args) => (
+  <EmployeeCard
+    name="John Caboosy"
+    company="A Virtual Friend"
+    phone="+31634XXX366"
+    image={require("./assets/employee1.png")}
+    mail="johncaboosy@virtualfriends.dev"
+    github="https://github.com/moesmoesie"
+    linkedIn="https://www.linkedin.com/in/mustafadarwesh/"
+  />
+);
+
+export const EmployeeCard1 = Template.bind({});
+
+EmployeeCard1.parameters = {
+  design: {
+    type: "figma",
+    url: "https://www.figma.com/file/WUYty59y1om6I6ejBzXVER/Virtual-Friends-Video-Components?node-id=3%3A39",
+  },
+};
---
+++ packages/ui/components/EmployeeCard/EmployeeCard.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
+
+export interface EmployeeCardProps {
+  name: string;
+  company: string;
+  phone?: string;
+  image: string;
+  github?: string;
+  linkedIn?: string;
+  mail: string;
+}
+
+const EmployeeCard: React.FC<EmployeeCardProps> = (props) => {
+  return (
+    <div className="relative flex h-72 w-56 items-end overflow-hidden rounded-lg px-6 py-5 shadow-2xl">
+      {/* Employee Image */}
+      <img
+        className="absolute left-0 top-0 h-full w-full bg-cover"
+        src={props.image}
+      />
+
+      <div className="z-10 flex w-full items-end justify-between">
+        {/* Employee Base Info */}
+        <div className="flex flex-col text-white">
+          <span className="text-lg font-bold text-white">{props.name}</span>
+          <span className="-mt-1 text-sm text-[#40C9A2]">{props.company}</span>
+          <span className="mt-1 text-sm text-white">{props.phone}</span>
+        </div>
+
+        {/* Employee Socials */}
+        <div className=" mb-[2px] flex flex-col gap-2">
+          {/* Github Button */}
+          {props.github && (
+            <a href={props.github}>
+              <FaGithub size={15} fill="white" />
+            </a>
+          )}
+
+          {/* LinkedIn Button */}
+          {props.linkedIn && (
+            <a href={props.linkedIn}>
+              <FaLinkedin size={15} fill="white" />
+            </a>
+          )}
+
+          {/* Mail Button */}
+          {props.mail && (
+            <a type="mail" href={props.mail}>
+              <FaEnvelope size={15} fill="white" />
+            </a>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EmployeeCard;
